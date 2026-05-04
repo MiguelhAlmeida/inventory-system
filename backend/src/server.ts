849cc5566da20a6e45bc70a3a7b67079feafd16d
Miguel Almeida <miguelh.almeida05@gmail.com>
2026-05-04 15:40:55 -0300

    feat: migrate project to typescript

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Servidor rodando");
+    res.send("Servidor com TypeScript");
 });
 
 app.listen(3000, () => {
