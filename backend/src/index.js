7f322ee23cf28bb38766f56dbe8efd4b9062d1e6
Miguel Almeida <miguelh.almeida05@gmail.com>
2026-05-04 15:12:50 -0300

    feat: create express server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Miguel");
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Servidor rodando");
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000")
+});
