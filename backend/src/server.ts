573d47b91444b71bf62b116f137fe31fd30b16d6
Miguel Almeida <miguelh.almeida05@gmail.com>
2026-05-04 15:45:46 -0300

    refactor: project structure with routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import router from "./routes";
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Servidor com TypeScript");
-});
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000")
