044bc831bd7cc1fec9bdd168e4f4de83580de2f2
Miguel Almeida <miguelh.almeida05@gmail.com>
2026-05-06 18:07:07 -0300

    feat: add logger middleware

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import productRoutes from "./routes/product.routes";
-import { json } from "node:stream/consumers";
+import { loggerMiddleware } from "./middlewares/logger.middleware";
 
 const app = express();
 
 app.use(express.json());
+app.use(loggerMiddleware);
 app.use(productRoutes);
 
 app.listen(3000, () => {
