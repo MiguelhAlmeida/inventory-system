45f91a106e1802834c45a4efeec19240ac22d66d
Miguel Almeida <miguelh.almeida05@gmail.com>
2026-05-19 19:46:28 -0300

    Revert "feat: implement global erro handling"
    This reverts commit e01dd137fc73bd4ba3d833c691ca2535f079a9e6.

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,120 +1,84 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 import { productService } from "../services/product.service";
-import { AppError } from "../errors/app-error";
 
 export const productController = {
     
-    getAll: (req: Request, res: Response, next: NextFunction) => {
-        
-        try {
-            const products = productService.getAll();
-
-            return res.json(products);
-
-        } catch (error) {
-            next(error);
-        }
-        
+    getAll: (req: Request, res: Response) => {
+        const products = productService.getAll();
+        return res.json(products);
     },
 
-    getById: (req: Request, res: Response, next: NextFunction) => {
-        
-        try{
-            const id = Number(req.params.id);
+    getById: (req: Request, res: Response) => {
+        const id = Number(req.params.id);
 
-            if (isNaN(id)) {
-                throw new AppError("ID Inválido", 400);
-            }
-
-            const product = productService.getById(id)
-
-            if (!product) {
-                throw new AppError("Produto não encontrado", 404);
-            }
-
-            return res.json(product);
-
-        } catch(error) {
-            next(error);
+        if (isNaN(id)) {
+            return res.status(400).json({ message: "ID inválido" });
         }
-        
+
+        const product = productService.getById(id)
+
+        if (!product) {
+            return res.status(404).json({ message: "Produto não encontrado" });
+        }
+
+        return res.json(product);
     },
 
-    create: (req: Request, res: Response, next: NextFunction) => {
+    create: (req: Request, res: Response) => {
+        const { name, price } = req.body;
 
-        try{
-            const { name, price } = req.body;
-
-            if (!name || typeof name !== "string") {
-                throw new AppError("Nome inválido", 400);
-            }
-
-            if (typeof price !== "number" || price <= 0) {
-                throw new AppError("Preço inválido", 400);
-            }
-
-            const newProduct = productService.create(name, price);
-
-            return res.status(201).json(newProduct);
-            
-        } catch (error) {
-            next(error);
+        if (!name || typeof name !== "string") {
+            return res.status(400).json({ message: "Nome inválido" })
         }
-        
+
+        if (typeof price !== "number" || price <= 0) {
+            return res.status(400).json({ message: "Preço inválido" })
+        }
+
+        const newProduct = productService.create(name, price);
+
+        return res.status(201).json(newProduct);
     },
 
-    update: (req: Request, res: Response, next: NextFunction) => {
+    update: (req: Request, res: Response) => {
+        const id = Number(req.params.id);
 
-        try {
-            const id = Number(req.params.id);
-
-            if (isNaN(id)) {
-                throw new AppError("ID Inválido", 400);
-            }
-
-            const { name, price } = req.body;
-
-            if (!name || typeof name !== "string") {
-                throw new AppError("Nome inválido", 400);
-            }
-
-            if (typeof price !== "number" || price <= 0) {
-                throw new AppError("Preço inválido", 400);
-            }
-
-            const updatedProduct = productService.update(id, name, price);
-
-            if (!updatedProduct) {
-                throw new AppError("Produto não encontrado", 404);
-            }
-
-            return res.json(updatedProduct);
-
-        } catch (error) {
-            next(error);
+        if (isNaN(id)) {
+            return res.status(400).json({ message: "ID inválido" });
         }
-        
+
+        const { name, price } = req.body;
+
+        if (!name || typeof name !== "string") {
+            return res.status(400).json({ message: "Nome inválido" });
+        }
+
+        if (typeof price !== "number" || price <= 0) {
+            return res.status(400).json({ message: "Preço inválido" });
+        }
+
+        const updatedProduct = productService.update(id, name, price);
+
+        if (!updatedProduct) {
+            return res.status(404).json({ message: "Produto não encontrado" });
+        }
+
+        return res.json(updatedProduct);
     },
 
-    delete: (req: Request, res: Response, next: NextFunction) => {
+    delete: (req: Request, res: Response) => {
+        const id = Number(req.params.id);
 
-        try {
-            const id = Number(req.params.id);
-
-            if (isNaN(id)) {
-                throw new AppError("ID Inválido", 400);
-            }
-
-            const deleted = productService.delete(id);
-
-            if (!deleted) {
-                throw new AppError("Produto não encontrado", 404);
-            }
-
-            return res.status(204).send();
-        } catch (error) {
-            next(error);
+        if (isNaN(id)) {
+            return res.status(400).json({ message: "ID inválido" });
         }
-        
+
+        const deleted = productService.delete(id);
+
+        if (!deleted) {
+            return res.status(404).json({ message: "Produto não encontrado" })
+        }
+
+        return res.status(204).send();
     }
 };
